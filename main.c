3ab267b32a9a7d225667420277879170ca1094d5
Guy SHARONY <gsharony@e2r10p18.42.fr>
2019-10-23 13:37:34 +0200

    Update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 10:20:28 by gsharony          #+#    #+#             */
-/*   Updated: 2019/10/23 13:15:56 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/10/23 13:37:10 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,16 @@
 
 int		main(void)
 {
-	int		a;
-	a = 3;
-	ft_printf("String: |%s|\nCharacter: |%c|\nNumber: |%d|\nOctet: |%i|\nHexa :|%i|\nAdresse: |%p|\n", "Middle", 'a', 10, 0125, 0x125, &a);
+	int				a;
+	unsigned int	b;
+
+	a = -42;
+	b = a;
+	ft_printf("String------|%s|\n", "Middle");
+	ft_printf("Character---|%c|\n", 'a');
+	ft_printf("Number------|%d|\n", 42);
+	ft_printf("Octet-------|%i|\n", 042);
+	ft_printf("Hexa--------|%i|\n", 0x125);
+	ft_printf("Adresse-----|%p|\n", &a);
 	return (0);
 }
