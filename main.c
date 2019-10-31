9e323fa5eefc995b908d3571a14f8afcec7702b8
Guy SHARONY <gsharony@e2r10p18.42.fr>
2019-10-31 16:06:20 +0100

    Flag testing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 10:20:28 by gsharony          #+#    #+#             */
-/*   Updated: 2019/10/31 15:31:12 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/10/31 16:05:47 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ int		main(void)
 	ft_printf("Unsigned HEX--|%X|\n", 450);
 	ft_printf("\n___ Testing [formats with flags] ___\n");
 	ft_printf("Number flag---|%-+508.10d|\n", -42);
-	printf("Number flag---|%-+508.10d|\n", -42);
+	ft_printf("|%%+508.10d|--|%+508.10d|\n", -42);
 	return (0);
 }
