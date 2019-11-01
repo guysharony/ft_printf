4be6e53b98017953a36f96d1aaec5a80f3afcac0
Guy SHARONY <gsharony@e3r10p18.42.fr>
2019-11-01 13:07:49 +0100

    Corrections in dsp_string.c

--- display/dsp_string.c
+++ display/dsp_string.c
@@ -6,44 +6,48 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 11:36:30 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/01 12:47:49 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/01 13:06:40 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
-#include <stdio.h>
 
-static void		print_flags(t_format f, char *str)
+static int		ft_space(t_format f, char *str)
+{
+	if (f.pr > 0 && (f.pr < (int)ft_strlen(str) || f.pr < f.wi))
+		return (f.wi - f.pr);
+	return (f.wi - ft_strlen(str));
+}
+
+static int		ft_string(t_format f, char *str)
+{
+	if (f.pr < 0)
+		return (ft_strlen(str));
+	return (f.pr);
+}
+
+void			dsp_char(t_format f, int c)
 {
 	int		a;
 
-	if (f.pr > 0 && (f.pr < (int)ft_strlen(str) || f.pr < f.wi))
-		a = f.wi - f.pr;
-	else if (f.pr <= 0 || f.pr > f.wi)
-		a = f.wi - ft_strlen(str);
+	a = f.wi - 1;
 	if (!ft_format('-', f.fl))
 		ft_time(' ', a);
-	if (f.pr < 0)
-		ft_putnstr(str, ft_strlen(str));
-	else
-		ft_putnstr(str, f.pr);
-	if (ft_format('-', f.fl))
-		ft_time(' ', a);
-}
-
-void	dsp_char(t_format f, int c)
-{
-	if (f.pr >= 0)
-		f.pr = -1;
-	f.wi--;
-	if (!ft_format('-', f.fl))
-		ft_time(' ', f.wi);
 	write(1, &c, 1);
 	if (ft_format('-', f.fl))
-		ft_time(' ', f.wi);
+		ft_time(' ', a);
 }
 
-void	dsp_str(t_format f, char *str)
+void			dsp_str(t_format f, char *str)
 {
-	print_flags(f, str);
+	int		a;
+	int		b;
+
+	a = ft_space(f, str);
+	b = ft_string(f, str);
+	if (!ft_format('-', f.fl))
+		ft_time(' ', a);
+	ft_putnstr(str, b);
+	if (ft_format('-', f.fl))
+		ft_time(' ', a);
 }
