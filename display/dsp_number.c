1bc2c4dd8472f6b7845232617c8fdac8362a41d4
Guy SHARONY <gsharony@e3r10p18.42.fr>
2019-11-01 13:52:48 +0100

    Flags for %d

--- display/dsp_number.c
+++ display/dsp_number.c
@@ -6,28 +6,31 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 07:04:07 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/01 10:47:51 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/01 13:49:52 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-void	ft_time(char c, int n)
+static int		ft_space(t_format f, int nb)
 {
-	while (n-- > 0)
-		write(1, &c, 1);
+	int		a;
+
+	a = f.wi;
+	if (f.pr > 0 && (f.pr < (int)ft_nbrlen(nb, 10) || f.pr < f.wi))
+		a -= f.pr;
+	else
+		a -= ft_nbrlen(nb, 10);
+	if (ft_format('+', f.fl))
+		return (a - 1);
+	return (a);
 }
 
-void	dsp_number(t_format f, long long nb)
+static int		ft_zero(t_format f, int nb)
 {
 	int		a;
 
 	a = 0;
-	f.wi -= f.pr + ft_nbrlen(nb, 10);
-	if (nb < 0)
-		f.wi++;
-	if (!ft_format('-', f.fl))
-		ft_time(' ', f.wi);
 	if (f.pr >= 0)
 	{
 		if (nb < 0)
@@ -35,13 +38,32 @@ void	dsp_number(t_format f, long long nb)
 			nb *= -1;
 			write(1, "-", 1);
 		}
-		while (a++ < f.pr - ft_nbrlen(nb, 10))
-			write(1, "0", 1);
+		a = f.pr - ft_nbrlen(nb, 10);
+		ft_time('0', a);
 	}
+	return (a);
+}
+
+void			ft_time(char c, int n)
+{
+	while (n-- > 0)
+		write(1, &c, 1);
+}
+
+void			dsp_number(t_format f, long long nb)
+{
+	int		a;
+
+	a = ft_space(f, nb);
+	if (!ft_format('-', f.fl))
+		ft_time(' ', a);
+	if (ft_format('+', f.fl))
+		write(1, "+", 1);
+	ft_zero(f, nb);
 	if (f.pr < 0)
 		ft_putnbr_base(nb, "0123456789");
 	else
 		ft_putnbr_base(nb / (10 * f.pr), "0123456789");
 	if (ft_format('-', f.fl))
-		ft_time(' ', f.wi);
+		ft_time(' ', a);
 }
