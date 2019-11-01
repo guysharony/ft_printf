2413d74c5e9b3cfba9eda667445b8381d6aacca6
Guy SHARONY <gsharony@e2r10p18.42.fr>
2019-11-01 07:55:23 +0100

    Adding tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 10:20:28 by gsharony          #+#    #+#             */
-/*   Updated: 2019/10/31 16:05:47 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/01 07:55:05 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,77 @@
 
 int		main(void)
 {
-	int a;
+	int		a;
+	int		b;
 
 	a = -42;
 	printf("\n___ Testing [simple output] ___\n");
-	ft_printf("Display 1p----|%%|\n");
-	ft_printf("Display 2p----|%%%%|\n");
-	ft_printf("Display 3p----|%%%%%%|\n");
-	printf("\n___ Testing [only formats] ___\n");
-	ft_printf("String--------|%s|\n", "Middle");
-	ft_printf("Character-----|%c|\n", 'a');
-	ft_printf("Number--------|%d|\n", 42);
-	ft_printf("Octet---------|%i|\n", 042);
-	ft_printf("Hexa----------|%i|\n", 0x125);
-	ft_printf("Adresse-------|%p|\n", &a);
-	ft_printf("Unsigned int--|%u|\n", -456);
-	ft_printf("Unsigned hex--|%x|\n", 450);
-	ft_printf("Unsigned HEX--|%X|\n", 450);
-	ft_printf("\n___ Testing [formats with flags] ___\n");
-	ft_printf("Number flag---|%-+508.10d|\n", -42);
-	ft_printf("|%%+508.10d|--|%+508.10d|\n", -42);
+	ft_printf("");
+	ft_printf("---|coco\net titi |\n");
+
+	printf("\n___ Testing [%%c] ___ \n");
+	ft_printf("---|%c|\n", 'a');
+	ft_printf("---|%c|\n", '\0');
+	ft_printf("---|%c|\n", (char)0xff11ff11);
+	ft_printf("---|%c|\n", -21);
+	ft_printf("---|%4c|\n", 'U');
+	ft_printf("---|%-1c|\n", 12);
+	ft_printf("---|%-12c|\n", '\n');
+	ft_printf("---|hello ca%----4c %1c va %10c%-c ??|\n", '\0', '\n', (char)564, 0);
+
+	printf("\n___ Testing [%%s] ___\n");
+	ft_printf("---|%s|\n", NULL);
+	ft_printf("---|%s|\n", "coco");
+	ft_printf("---|%s|\n", "");
+	ft_printf("---|%.15s|\n", "coco");
+	ft_printf("---|%.5s|\n", "coco et titi");
+	ft_printf("---|%.0s|\n", "coco et ti");
+	ft_printf("---|%.s|\n", "coco");
+	ft_printf("---|%.2s|\n", NULL);
+	ft_printf("---|%10s|\n", NULL);
+	ft_printf("---|%-8s|\n", "coco");
+	ft_printf("---|%1s|\n", "hi");
+	ft_printf("---|%-2s|\n", "");
+	ft_printf("---|Coucou %.0s\n%s%---12s|\n", "hi", "coco", NULL);
+	ft_printf("---|u%4.2ss %-1.s\n %---5.3s|\n", "coco", NULL, "yooo");
+	ft_printf("---|%1.4s et %-6.8s et %4.2s|\n", "tuuu", "12345", "hu");
+
+	printf("\n___ Testing [%%p] ___\n");
+	ft_printf("---|%p|\n", NULL);
+	ft_printf("---|%p|\n", &a);
+	ft_printf("---|%20p|\n", &a);
+	ft_printf("---|%-25p|\n", &a);
+	ft_printf("---|%2p|\n", &a);
+	ft_printf("---|%----24p et hello %2p %12p|\n", &b, &a, NULL);
+
+	printf("\n___ Testing [%%d] ___\n");
+	ft_printf("---|%d|\n", 42);
+	ft_printf("---|%d|\n", -42);
+	ft_printf("---|%d|\n", 0);
+	ft_printf("---|%d|\n", 3);
+	ft_printf("---|%d|\n", -2147483647);
+	ft_printf("---|% d|\n", 0);
+	ft_printf("---|%+d|\n", 0);
+	ft_printf("---|%+++d|\n", 534);
+	ft_printf("---|% d|\n", 0xff11ff);
+	ft_printf("---|%.12d|\n", 1144);
+	ft_printf("---|%.d|\n", 0);
+	ft_printf("---|%-7d|\n", 7789);
+	ft_printf("---|%0d|\n", -579);
+	ft_printf("---|%04d|\n", 0);
+	ft_printf("---|%+12.5d|\n", 140);
+	ft_printf("---|%00+10.4d|\n", 0);
+	ft_printf("---|%.0d|\n", -21);
+	ft_printf("---|%.5d|\n", -421);
+	ft_printf("---|%5d|\n", -741);
+	ft_printf("---|hello % .d this is a test|\n", 0);
+
+	printf("\n___ Testing [%%i] ___\n");
+	ft_printf("---|%i|\n", (signed int)0xff11);
+	ft_printf("---|%.2i|\n", -10);
+	ft_printf("---|%.i|\n", 44);
+	ft_printf("---|%08i|\n", -71);
+	ft_printf("---|%-2i|\n", -7);
+	ft_printf("---|%.0i|\n", 0);
 	return (0);
 }
