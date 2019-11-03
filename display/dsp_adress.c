43433439a1c8b93c17628d0f527192690d4fcdf3
Guy SHARONY <gsharony@e3r10p18.42.fr>
2019-11-03 07:42:36 +0100

    Correcting adresses

--- display/dsp_adress.c
+++ display/dsp_adress.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 11:57:19 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/01 10:59:27 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/03 07:41:44 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	dsp_adress(t_format f, void *str)
 	if (f.pr >= 0)
 		return ;
 	f.wi -= ft_nbrlen((long long)str, 16) + 2;
-	if (!str)
-		f.wi -= 1;
 	if (!ft_format('-', f.fl))
 		ft_time(' ', f.wi);
 	ft_print_adress(str);
