4c024b151b24c169c1861693292192a5eb8408c6
Guy SHARONY <gsharony@e3r11p14.42.fr>
2019-11-06 08:57:18 +0100

    Other errors in 'c'

--- error/err_string.c
+++ error/err_string.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 08:18:23 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/06 08:31:14 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/06 08:57:01 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 int		err_char(t_format f, int c)
 {
 	(void)c;
-	if (f.pr >= 0 || ft_format('+', f.fl))
+	if (f.pr >= 0 || ft_format('+', f.fl) || ft_format('0', f.fl))
+		return (1);
+	if (ft_format(' ', f.fl))
 		return (1);
 	return (0);
 }
