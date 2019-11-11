b60f93c3cd993a1377f136b81f5c9ee7c603f483
Guy SHARONY <gsharony@e3r11p14.42.fr>
2019-11-11 12:12:54 +0100

    Way better

--- display/dsp_unsigned.c
+++ display/dsp_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 12:57:46 by gsharony          #+#    #+#             */
-/*   Updated: 2019/11/11 11:22:01 by gsharony         ###   ########.fr       */
+/*   Updated: 2019/11/11 12:12:32 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static unsigned int		before_unsigned(unsigned int nb, int len, t_format f)
 		}
 		else if (ft_format('-', f.fl))
 		{
-			if (ft_format('0', f.fl) && f.pr < 0)
+			if (ft_format('0', f.fl) && f.pr <= 0)
 			{
 				nb = sign_unsigned(nb);
 				ft_time('0', f.wi - len);
@@ -52,10 +52,10 @@ int						dsp_unsigned(t_format f, unsigned int nb, char *base)
 {
 	int		len;
 
-	len = ft_size_unsigned(nb, base, f);
+	len = 0;
+	len += ft_size_unsigned(nb, base, f);
 	nb = before_unsigned(nb, len, f);
 	ft_print_unsigned(nb, base, f);
-	printf("[%d] - [%d] - [%d] - [%s]\n", f.wi, f.pr, len, f.fl);
 	if (f.wi > len)
 	{
 		if (ft_format('-', f.fl))
