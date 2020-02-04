fa6b5d3204f24736feb4cbaea5365f11bd1a8e38
Guy SHARONY <gsharony@e2r12p4.42.fr>
2020-02-04 13:15:56 +0100

    Deleting everything

--- compile.sh
+++
@@ -1,8 +0,0 @@
-printf "\033[1;32m===== creating executable =====\n"
-make
-gcc main.c libftprintf.a
-printf "\n\033[1;37m===== output =====\n"
-./a.out | less
-printf "\n\033[1;32m===== cleaning =====\n"
-make fclean
-printf "\033[0m"
